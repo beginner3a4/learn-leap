bc10150309fcb257f33cab39fa5d616b1d979f18
Bhushan <pardeepdpolice6@gmail.com>
2025-09-13 13:08:08 +0530

    Configure for production deployment

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [
-    react(), tailwindcss(),
-  ],
-})
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "dist",
+    sourcemap: false,
+    minify: true,
+    chunkSizeWarningLimit: 1600,
+  },
+  server: {
+    port: process.env.PORT || 3000,
+    host: true,
+  },
+});
